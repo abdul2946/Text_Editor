94136d1ef41b6689ef302c14b7d192993dea6a9a
abdul2946 <aazim3510@gmail.com>
2021-11-30 20:46:56 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,112 @@
+// Home Secion
+
+// Styling
+const boldbtn = document.querySelector("#bold-btn");
+const italicbtn = document.querySelector("#italic-btn");
+const underlinebtn = document.querySelector("#underline-btn");
+const strikebtn = document.querySelector("#strike-btn");
+const quotebtn = document.querySelector("#quote-btn");
+const paragraphbtn = document.querySelector("#paragraph-btn");
+const superscriptbtn = document.querySelector("#superscript-btn");
+const subscriptbtn = document.querySelector("#subscript-btn");
+
+boldbtn.addEventListener("click", () => {
+  document.execCommand("bold");
+});
+italicbtn.addEventListener("click", () => {
+  document.execCommand("italic");
+});
+underlinebtn.addEventListener("click", () => {
+  document.execCommand("underline");
+});
+strikebtn.addEventListener("click", () => {
+  document.execCommand("strikeThrough");
+});
+quotebtn.addEventListener("click", () => {});
+paragraphbtn.addEventListener("click", () => {
+  document.execCommand("insertParagraph");
+});
+superscriptbtn.addEventListener("click", () => {
+  document.execCommand("superscript");
+});
+subscriptbtn.addEventListener("click", () => {
+  document.execCommand("subscript");
+});
+
+//list
+const ulbtn = document.querySelector("#ul-btn");
+const olbtn = document.querySelector("#ol-btn");
+
+ulbtn.addEventListener("click", () => {
+  document.execCommand("insertUnorderedList");
+});
+olbtn.addEventListener("click", () => {
+  document.execCommand("insertOrderedList");
+});
+
+//Color
+const frontcolorbtn = document.querySelector("#forcolor-btn");
+const backcolorbtn = document.querySelector("#backcolor-btn");
+frontcolorbtn.addEventListener("input", () => {
+  document.execCommand("forecolor", false, frontcolorbtn.value);
+});
+backcolorbtn.addEventListener("input", () => {
+  document.execCommand("backColor", false, backcolorbtn.value);
+});
+
+// Alignment
+const alignLeft = document.querySelector("#justify-left");
+const alignCenter = document.querySelector("#justify-center");
+const alignRight = document.querySelector("#justify-right");
+const alignFull = document.querySelector("#justify-full");
+const outBtn = document.querySelector("#indent-left");
+const inBtn = document.querySelector("#indent-right");
+
+alignLeft.addEventListener("click", () => {
+  document.execCommand("justifyLeft");
+});
+alignCenter.addEventListener("click", () => {
+  document.execCommand("justifyCenter");
+});
+alignRight.addEventListener("click", () => {
+  document.execCommand("justifyRight");
+});
+alignFull.addEventListener("click", () => {
+  document.execCommand("justifyFull");
+});
+inBtn.addEventListener("click", () => {
+  document.execCommand("indent");
+});
+outBtn.addEventListener("click", () => {
+  document.execCommand("outdent");
+});
+
+
+
+
+// Edit Section
+const cutbtn = document.querySelector("#cut-btn");
+const copybtn = document.querySelector("#copy-btn");
+const pestbtn = document.querySelector("#pest-btn");
+const undobtn = document.querySelector("#undo-btn");
+const redobtn = document.querySelector("#redo-btn");
+
+cutbtn.addEventListener("click", () => {
+  document.execCommand("cut");
+});
+copybtn.addEventListener("click", () => {
+  document.execCommand("copy");
+});
+pestbtn.addEventListener("click", () => {
+  document.execCommand("paste");
+});
+undobtn.addEventListener("click", () => {
+  document.execCommand("undo");
+});
+redobtn.addEventListener("click", () => {
+  document.execCommand("redo");
+});
+
+
+
+// Insert Section
---
+++ tooltip.js
@@ -0,0 +1,116 @@
+
+// Home
+tippy("#heading-btn", {
+  content: "Font Types",
+  placement: "bottom",
+  theme: "my-theme",
+  arrow: false,
+});
+tippy("#style-btn", {
+  content: "Font Styles",
+  placement: "bottom",
+  theme: "my-theme",
+  arrow: false,
+});
+tippy("#bold-btn", {
+  content: "Bold",
+  placement: "bottom",
+  theme: "my-theme",
+  arrow: false,
+});
+tippy("#italic-btn", {
+  content: "Italic",
+  placement: "bottom",
+  theme: "my-theme",
+  arrow: false,
+});
+tippy("#underline-btn", {
+  content: "Underline",
+  placement: "bottom",
+  theme: "my-theme",
+  arrow: false,
+});
+tippy("#strike-btn", {
+  content: "Strikethrough",
+  placement: "bottom",
+  theme: "my-theme",
+  arrow: false,
+});
+tippy("#quote-btn", {
+  content: "Quote",
+  placement: "bottom",
+  theme: "my-theme",
+  arrow: false,
+});
+tippy("#paragraph-btn", {
+  content: "Paragraph",
+  placement: "bottom",
+  theme: "my-theme",
+  arrow: false,
+});
+tippy("#superscript-btn", {
+  content: "Superscript",
+  placement: "bottom",
+  theme: "my-theme",
+  arrow: false,
+});
+tippy("#subscript-btn", {
+  content: "Subscript",
+  placement: "bottom",
+  theme: "my-theme",
+  arrow: false,
+});
+tippy("#ul-btn", {
+  content: "Unorderd List",
+  placement: "bottom",
+  theme: "my-theme",
+  arrow: false,
+});
+tippy("#ol-btn", {
+  content: "Ordered List",
+  placement: "bottom",
+  theme: "my-theme",
+  arrow: false,
+});
+tippy("#color-btn", {
+  content: "Color Picker",
+  placement: "bottom",
+  theme: "my-theme",
+  arrow: false,
+});
+tippy("#justify-left", {
+  content: "justify Left",
+  placement: "bottom",
+  theme: "my-theme",
+  arrow: false,
+});
+tippy("#justify-center", {
+  content: "Justify Center",
+  placement: "bottom",
+  theme: "my-theme",
+  arrow: false,
+});
+tippy("#justify-right", {
+  content: "Justify Right",
+  placement: "bottom",
+  theme: "my-theme",
+  arrow: false,
+});
+tippy("#justify-full", {
+  content: "Justify Full",
+  placement: "bottom",
+  theme: "my-theme",
+  arrow: false,
+});
+tippy("#indent-left", {
+  content: "Outdent",
+  placement: "bottom",
+  theme: "my-theme",
+  arrow: false,
+});
+tippy("#indent-right", {
+  content: "Indent",
+  placement: "bottom",
+  theme: "my-theme",
+  arrow: false,
+});
